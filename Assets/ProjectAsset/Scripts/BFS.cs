d0527a88cd02a26bdff123c8ec3153864c9a0145
BahaaSrour <dev.bahaasrour@gmail.com>
2021-03-13 22:27:35 +0200

    -Now the path is solved and drown

---
+++ Assets/ProjectAsset/Scripts/BFS.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BFS : MonoBehaviour
+{
+    [SerializeField] GridSpawner gs;
+    [SerializeField] List<GridType> visitedGridsList;
+    [SerializeField] int playerPosX;
+    [SerializeField] int playerPosZ;
+    public int gridLength;
+    [SerializeField] int X_goal;
+    [SerializeField] int Z_goal;
+    [SerializeField] GameObject Pathprefab;
+    void Start()
+    { 
+        Visited = new Queue<GridType>(); 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+            for (int i = 0; i < gs.gridX; i++)
+            {
+                for (int j = 0; j < gs.gridZ; j++)
+                {
+                    Debug.Log(string.Format("item [{0},{1}]", i, j, gs.itemToSpwan[i, j]));
+                }
+            }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log(gs.itemToSpwan[0, 0]);
+            bfs(gs.itemToSpwan[0, 0], gs.itemToSpwan[X_goal, Z_goal]);
+      
+        }
+    }
+
+    bool tergetFound;
+    public void bfs(GridType startNode )
+    {
+        Visited.Enqueue(startNode);
+        if (startNode.gridWalkabliliyState == GridWalkabliliyState.unwalkable)
+        {
+            Debug.Log("Cant Move");
+            return;
+        }
+        while (Visited.Count>0)
+        {
+            if (tergetFound) break;
+            Debug.Log("Visited "+Visited.Count);
+            tmp = Visited.Dequeue();
+
+            Solve(X_goal, Z_goal);
+        }
+        Debug.Log("no soulutoin");
+        if (tergetFound) DrawPath(tmp);
+    }
+
+    void DrawPath(GridType Target)
+    {
+        while (tmp.parentInGrid != null)
+        {
+            GameObject.Instantiate(Pathprefab, tmp.CellType.transform.position, Quaternion.identity);
+            tmp = tmp.parentInGrid;
+        }
+
+    }
+    bool goalHasFound;
+    public Queue<GridType> Visited;
+    GridType tmp;
+
+    //search if i can reach the nood or not
+    void Solve(/*int X_index, int Z_index,*/ int Xgoal, int Zgoal)
+    {
+        if (!(tmp.Xindex == Xgoal && tmp.Zindex == Zgoal))
+        {
+            tmp.Visited = true;
+            //Check up
+            if (tmp.Xindex > 0 && !gs.itemToSpwan[tmp.Xindex - 1, tmp.Zindex].Visited && gs.itemToSpwan[tmp.Xindex - 1, tmp.Zindex].gridWalkabliliyState == GridWalkabliliyState.walkable)
+            {
+                if (tmp.Xindex - 1 == Xgoal && tmp.Zindex == Zgoal) goalHasFound = true;
+                Visited.Enqueue(gs.itemToSpwan[tmp.Xindex - 1, tmp.Zindex]);
+                gs.itemToSpwan[tmp.Xindex - 1, tmp.Zindex].parentInGrid = tmp;
+                gs.itemToSpwan[tmp.Xindex - 1, tmp.Zindex].weight = tmp.weight + 1;
+                gs.itemToSpwan[tmp.Xindex - 1, tmp.Zindex].Visited = true;
+            }
+
+            //check Down 
+            if (tmp.Xindex < gs.gridX - 1 && !gs.itemToSpwan[tmp.Xindex + 1, tmp.Zindex].Visited && gs.itemToSpwan[tmp.Xindex + 1, tmp.Zindex].gridWalkabliliyState == GridWalkabliliyState.walkable)
+            {
+                if (tmp.Xindex + 1 == Xgoal && tmp.Zindex == Zgoal) goalHasFound = true;
+                Visited.Enqueue(gs.itemToSpwan[tmp.Xindex + 1, tmp.Zindex]);
+                gs.itemToSpwan[tmp.Xindex + 1, tmp.Zindex].parentInGrid = tmp;
+                gs.itemToSpwan[tmp.Xindex + 1, tmp.Zindex].weight = tmp.weight + 1;
+                gs.itemToSpwan[tmp.Xindex + 1, tmp.Zindex].Visited = true;
+            }
+
+            //check Right
+            if (tmp.Zindex < gs.gridZ - 1 && !gs.itemToSpwan[tmp.Xindex, tmp.Zindex + 1].Visited && gs.itemToSpwan[tmp.Xindex, tmp.Zindex + 1].gridWalkabliliyState == GridWalkabliliyState.walkable)
+            {
+                if (tmp.Xindex == Xgoal && tmp.Zindex + 1 == Zgoal) goalHasFound = true;
+                Visited.Enqueue(gs.itemToSpwan[tmp.Xindex, tmp.Zindex + 1]);
+                gs.itemToSpwan[tmp.Xindex, tmp.Zindex + 1].parentInGrid = tmp;
+                gs.itemToSpwan[tmp.Xindex, tmp.Zindex + 1].weight = tmp.weight + 1;
+                gs.itemToSpwan[tmp.Xindex, tmp.Zindex + 1].Visited = true;
+            }
+
+            //Check Left
+            if (tmp.Zindex > 0 && !gs.itemToSpwan[tmp.Xindex, tmp.Zindex - 1].Visited && gs.itemToSpwan[tmp.Xindex, tmp.Zindex - 1].gridWalkabliliyState == GridWalkabliliyState.walkable)
+            {
+                if (tmp.Xindex == Xgoal && tmp.Zindex - 1 == Zgoal) goalHasFound = true;
+                Visited.Enqueue(gs.itemToSpwan[tmp.Xindex, tmp.Zindex - 1]);
+                gs.itemToSpwan[tmp.Xindex, tmp.Zindex - 1].parentInGrid = tmp;
+                gs.itemToSpwan[tmp.Xindex, tmp.Zindex - 1].weight = tmp.weight + 1;
+                gs.itemToSpwan[tmp.Xindex, tmp.Zindex - 1].Visited = true;
+            }
+
+        }
+        else { Debug.Log("goalHasFound"); tergetFound = true; }
+        return;
+    }
+
+}
--- Assets/ProjectAsset/Scripts/GridSpawner.cs
+++ Assets/ProjectAsset/Scripts/GridSpawner.cs
@@ -10,8 +10,8 @@ public class GridSpawner : MonoBehaviour
     [SerializeField] int unWalkingGridNumber;
     [SerializeField] GameObject walkableGrid;
     [SerializeField] GameObject PathSolutionGrid;
-    [SerializeField] int gridX = 5;
-    [SerializeField] int gridZ = 6;
+    [SerializeField] public int gridX = 5;
+    [SerializeField] public int gridZ = 6;
     [SerializeField] Vector3 gridOrigin;
     [SerializeField] float gridOffset;
     [SerializeField] int Test_x;
@@ -20,7 +20,6 @@ public class GridSpawner : MonoBehaviour
     [HideInInspector] public int[,] GridValue;
     void Start()
     {
-
         GridValue = new int[gridX, gridZ];
         itemToSpwan = new GridType[gridX, gridZ];
         for (int i = 0; i < gridX; i++)
@@ -34,29 +33,27 @@ public class GridSpawner : MonoBehaviour
         SpawnGrid();
     }
     //For testing porpose
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.S))
-            for (int i = 0; i < gridX; i++)
-            {
-                for (int j = 0; j < gridZ; j++)
-                {
-                    Debug.Log(string.Format("item [{0},{1}]", i, j, itemToSpwan[i, j]));
-                }
-            }
-        if (Input.GetKeyDown(KeyCode.D))
-            Debug.Log(" value x " + itemToSpwan.Length.ToString());
-        if (Input.GetKeyDown(KeyCode.A))
-            Debug.Log(" value x " + itemToSpwan[Test_x, Test_z].gridWalkabliliyState.ToString());
-    }
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.S))
+    //        for (int i = 0; i < gridX; i++)
+    //        {
+    //            for (int j = 0; j < gridZ; j++)
+    //            {
+    //                Debug.Log(string.Format("item [{0},{1}]", i, j, itemToSpwan[i, j]));
+    //            }
+    //        }
+    //    if (Input.GetKeyDown(KeyCode.D))
+    //        Debug.Log(" value x " + itemToSpwan.Length.ToString());
+    //    if (Input.GetKeyDown(KeyCode.A))
+    //        Debug.Log(" value x " + itemToSpwan[Test_x, Test_z].gridWalkabliliyState.ToString()+ " ( "+ itemToSpwan[Test_x, Test_z] .Xindex+ " , "+ itemToSpwan[Test_x, Test_z] .Zindex+ " )");
+    //}
     void SpawnGrid()
     {
-        Debug.Log(" value x " + GridValue.Length.ToString());
         for (int i = 0; i < gridX; i++)
             for (int j = 0; j < gridZ; j++)
             {
                 Vector3 spawnObjPosition = new Vector3(i * gridOffset, .5f, j * gridOffset) + gridOrigin;
-                //  Debug.Log(" pos " + spawnObjPosition);
                 spawnObj(spawnObjPosition, unWalkableGrid, Quaternion.identity, i, j);
             }
     }
@@ -69,13 +66,13 @@ public class GridSpawner : MonoBehaviour
             unWalkingGridNumber--;
             cloneItem = GameObject.Instantiate(walkableGrid, objPos, objRotation, this.transform);
             cloneItem.name = string.Format("[{0},{1}]", X_Index, Z_Index);
-            itemToSpwan[X_Index, Z_Index].AddValues(cloneItem, GridWalkabliliyState.unwalkable);
+            itemToSpwan[X_Index, Z_Index].AddValues(cloneItem, GridWalkabliliyState.unwalkable, X_Index,Z_Index);
         }
         else
         {
             cloneItem = GameObject.Instantiate(unWalkableGrid, objPos, objRotation, this.transform);
             cloneItem.name = string.Format("[{0},{1}]", X_Index, Z_Index);
-            itemToSpwan[X_Index, Z_Index].AddValues(cloneItem, GridWalkabliliyState.walkable);
+            itemToSpwan[X_Index, Z_Index].AddValues(cloneItem, GridWalkabliliyState.walkable, X_Index, Z_Index);
         }
     }
 }
@@ -86,11 +83,19 @@ public enum GridWalkabliliyState
 }
 public class GridType
 {
+    public int weight;
+    public bool Visited;
     public GameObject CellType;
     public GridWalkabliliyState gridWalkabliliyState;
-    public void AddValues(GameObject obj, GridWalkabliliyState state)
+    public GridType parentInGrid =null;
+    public  int Xindex, Zindex;
+    public void AddValues(GameObject obj, GridWalkabliliyState state,int x,int z)
     {
+        Xindex = x;
+        Zindex = z;
+        Visited = false;
         CellType = obj;
         gridWalkabliliyState = state;
     }
+
 }
